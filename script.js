c09f2341eb1d2630c12c69349249ecfa258a61f3
MuhammadZaky2828 <konami8721@gmail.com>
2024-07-15 17:04:59 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const currentLocation = location.href;
+    const menuItem = document.querySelectorAll('nav ul li a');
+    const menuLength = menuItem.length;
+    for (let i = 0; i < menuLength; i++) {
+        if (menuItem[i].href === currentLocation) {
+            menuItem[i].className = "active";
+        }
+    }
+});
+
+function toggleDescription(element) {
+    const description = element.querySelector('.service-description');
+    if (description.style.display === 'none' || description.style.display === '') {
+        description.style.display = 'block';
+    } else {
+        description.style.display = 'none';
+    }
+}
